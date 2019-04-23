64e24d22fba969dbde145017f1a7391e812d515c
Daniel Zanchi <danny.zanchi@gmail.com>
2019-04-23 16:01:13 +0200

    added two projects (instaprank, myhours) cleaned JS

--- js/main.js
+++ js/main.js
@@ -75,283 +75,56 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-  $('#one').hover(
+  $('figure').hover(
     function() {
-      $('#one').css("z-index", "200")
-			$('#one').css("transform", "scale(1.14)")
-      $('#two').css("-webkit-filter", "grayscale(100%)")
-      $('#three').css("-webkit-filter", "grayscale(100%)")
-      $('#four').css("-webkit-filter", "grayscale(100%)")
-      $('#five').css("-webkit-filter", "grayscale(100%)")
-      $('#six').css("-webkit-filter", "grayscale(100%)")
-      $('#seven').css("-webkit-filter", "grayscale(100%)")
-      $('#eight').css("-webkit-filter", "grayscale(100%)")
+      $(this).css("z-index", "200")
+			$(this).css("transform", "scale(1.14)")
+			$('figure').css("-webkit-filter", "grayscale(100%)")
+			$(this).css("-webkit-filter", "grayscale(0%)")
     },
     function() {
-      $('#one').css("z-index", "2")
+      $(this).css("z-index", "2")
 			$(this).css("transform", "scale(1.0)")
-      $('#two').css("-webkit-filter", "grayscale(0%)")
-      $('#three').css("-webkit-filter", "grayscale(0%)")
-      $('#four').css("-webkit-filter", "grayscale(0%)")
-      $('#five').css("-webkit-filter", "grayscale(0%)")
-      $('#six').css("-webkit-filter", "grayscale(0%)")
-      $('#seven').css("-webkit-filter", "grayscale(0%)")
-      $('#eight').css("-webkit-filter", "grayscale(0%)")
+			$('figure').css("-webkit-filter", "grayscale(0%)")
+			$(this).css("-webkit-filter", "grayscale(0%)")
     }
   )
 });
 
-$(document).ready(function() {
-  $('#two').hover(
-    function() {
-      $('#two').css("z-index", "200")
-			$(this).css("transform", "scale(1.14)")
-      $('#one').css("-webkit-filter", "grayscale(100%)")
-      $('#three').css("-webkit-filter", "grayscale(100%)")
-      $('#four').css("-webkit-filter", "grayscale(100%)")
-      $('#five').css("-webkit-filter", "grayscale(100%)")
-      $('#six').css("-webkit-filter", "grayscale(100%)")
-      $('#seven').css("-webkit-filter", "grayscale(100%)")
-      $('#eight').css("-webkit-filter", "grayscale(100%)")
-    },
-    function() {
-      $('#two').css("z-index", "2")
-			$(this).css("transform", "scale(1.0)")
-      $('#one').css("-webkit-filter", "grayscale(0%)")
-      $('#three').css("-webkit-filter", "grayscale(0%)")
-      $('#four').css("-webkit-filter", "grayscale(0%)")
-      $('#five').css("-webkit-filter", "grayscale(0%)")
-      $('#six').css("-webkit-filter", "grayscale(0%)")
-      $('#seven').css("-webkit-filter", "grayscale(0%)")
-      $('#eight').css("-webkit-filter", "grayscale(0%)")
-    }
-  )
-});
-
-$(document).ready(function() {
-  $('#three').hover(
-    function() {
-      $('#three').css("z-index", "200")
-			$(this).css("transform", "scale(1.14)")
-      $('#one').css("-webkit-filter", "grayscale(100%)")
-      $('#two').css("-webkit-filter", "grayscale(100%)")
-      $('#four').css("-webkit-filter", "grayscale(100%)")
-      $('#five').css("-webkit-filter", "grayscale(100%)")
-      $('#six').css("-webkit-filter", "grayscale(100%)")
-      $('#seven').css("-webkit-filter", "grayscale(100%)")
-      $('#eight').css("-webkit-filter", "grayscale(100%)")
-    },
-    function() {
-      $('#three').css("z-index", "2")
-			$(this).css("transform", "scale(1.0)")
-      $('#one').css("-webkit-filter", "grayscale(0%)")
-      $('#two').css("-webkit-filter", "grayscale(0%)")
-      $('#four').css("-webkit-filter", "grayscale(0%)")
-      $('#five').css("-webkit-filter", "grayscale(0%)")
-      $('#six').css("-webkit-filter", "grayscale(0%)")
-      $('#seven').css("-webkit-filter", "grayscale(0%)")
-      $('#eight').css("-webkit-filter", "grayscale(0%)")
-    }
-  )
-});
-
-$(document).ready(function() {
-  $('#four').hover(
-    function() {
-      $('#four').css("z-index", "200")
-			$(this).css("transform", "scale(1.14)")
-      $('#one').css("-webkit-filter", "grayscale(100%)")
-      $('#two').css("-webkit-filter", "grayscale(100%)")
-      $('#three').css("-webkit-filter", "grayscale(100%)")
-      $('#five').css("-webkit-filter", "grayscale(100%)")
-      $('#six').css("-webkit-filter", "grayscale(100%)")
-      $('#seven').css("-webkit-filter", "grayscale(100%)")
-      $('#eight').css("-webkit-filter", "grayscale(100%)")
-    },
-    function() {
-      $('#four').css("z-index", "2")
-			$(this).css("transform", "scale(1.0)")
-      $('#one').css("-webkit-filter", "grayscale(0%)")
-      $('#two').css("-webkit-filter", "grayscale(0%)")
-      $('#three').css("-webkit-filter", "grayscale(0%)")
-      $('#five').css("-webkit-filter", "grayscale(0%)")
-      $('#six').css("-webkit-filter", "grayscale(0%)")
-      $('#seven').css("-webkit-filter", "grayscale(0%)")
-      $('#eight').css("-webkit-filter", "grayscale(0%)")
-    }
-  )
-});
-
-$(document).ready(function() {
-  $('#five').hover(
-    function() {
-      $('#five').css("z-index", "200")
-			$(this).css("transform", "scale(1.14)")
-      $('#one').css("-webkit-filter", "grayscale(100%)")
-      $('#two').css("-webkit-filter", "grayscale(100%)")
-      $('#three').css("-webkit-filter", "grayscale(100%)")
-      $('#four').css("-webkit-filter", "grayscale(100%)")
-      $('#six').css("-webkit-filter", "grayscale(100%)")
-      $('#seven').css("-webkit-filter", "grayscale(100%)")
-      $('#eight').css("-webkit-filter", "grayscale(100%)")
-    },
-    function() {
-      $('#five').css("z-index", "2")
-			$(this).css("transform", "scale(1.0)")
-      $('#one').css("-webkit-filter", "grayscale(0%)")
-      $('#two').css("-webkit-filter", "grayscale(0%)")
-      $('#three').css("-webkit-filter", "grayscale(0%)")
-      $('#four').css("-webkit-filter", "grayscale(0%)")
-      $('#six').css("-webkit-filter", "grayscale(0%)")
-      $('#seven').css("-webkit-filter", "grayscale(0%)")
-      $('#eight').css("-webkit-filter", "grayscale(0%)")
-    }
-  )
-});
-
-$(document).ready(function() {
-  $('#six').hover(
-    function() {
-      $('#six').css("z-index", "200")
-			$(this).css("transform", "scale(1.14)")
-      $('#one').css("-webkit-filter", "grayscale(100%)")
-      $('#two').css("-webkit-filter", "grayscale(100%)")
-      $('#three').css("-webkit-filter", "grayscale(100%)")
-      $('#four').css("-webkit-filter", "grayscale(100%)")
-      $('#five').css("-webkit-filter", "grayscale(100%)")
-      $('#seven').css("-webkit-filter", "grayscale(100%)")
-      $('#eight').css("-webkit-filter", "grayscale(100%)")
-    },
-    function() {
-      $('#six').css("z-index", "2")
-			$(this).css("transform", "scale(1.0)")
-      $('#one').css("-webkit-filter", "grayscale(0%)")
-      $('#two').css("-webkit-filter", "grayscale(0%)")
-      $('#three').css("-webkit-filter", "grayscale(0%)")
-      $('#four').css("-webkit-filter", "grayscale(0%)")
-      $('#five').css("-webkit-filter", "grayscale(0%)")
-      $('#seven').css("-webkit-filter", "grayscale(0%)")
-      $('#eight').css("-webkit-filter", "grayscale(0%)")
-    }
-  )
-});
-
-$(document).ready(function() {
-  $('#seven').hover(
-    function() {
-      $('#seven').css("z-index", "200")
-			$(this).css("transform", "scale(1.14)")
-      $('#one').css("-webkit-filter", "grayscale(100%)")
-      $('#two').css("-webkit-filter", "grayscale(100%)")
-      $('#three').css("-webkit-filter", "grayscale(100%)")
-      $('#four').css("-webkit-filter", "grayscale(100%)")
-      $('#five').css("-webkit-filter", "grayscale(100%)")
-      $('#six').css("-webkit-filter", "grayscale(100%)")
-      $('#eight').css("-webkit-filter", "grayscale(100%)")
-    },
-    function() {
-      $('#seven').css("z-index", "2")
-			$(this).css("transform", "scale(1.0)")
-      $('#one').css("-webkit-filter", "grayscale(0%)")
-      $('#two').css("-webkit-filter", "grayscale(0%)")
-      $('#three').css("-webkit-filter", "grayscale(0%)")
-      $('#four').css("-webkit-filter", "grayscale(0%)")
-      $('#five').css("-webkit-filter", "grayscale(0%)")
-      $('#six').css("-webkit-filter", "grayscale(0%)")
-      $('#eight').css("-webkit-filter", "grayscale(0%)")
-    }
-  )
-});
-
-$(document).ready(function() {
-  $('#eight').hover(
-    function() {
-      $('#eight').css("z-index", "200")
-			$(this).css("transform", "scale(1.14)")
-      $('#one').css("-webkit-filter", "grayscale(100%)")
-      $('#two').css("-webkit-filter", "grayscale(100%)")
-      $('#three').css("-webkit-filter", "grayscale(100%)")
-      $('#four').css("-webkit-filter", "grayscale(100%)")
-      $('#five').css("-webkit-filter", "grayscale(100%)")
-      $('#six').css("-webkit-filter", "grayscale(100%)")
-      $('#seven').css("-webkit-filter", "grayscale(100%)")
-    },
-    function() {
-      $('#eight').css("z-index", "2")
-			$(this).css("transform", "scale(1.0)")
-      $('#one').css("-webkit-filter", "grayscale(0%)")
-      $('#two').css("-webkit-filter", "grayscale(0%)")
-      $('#three').css("-webkit-filter", "grayscale(0%)")
-      $('#four').css("-webkit-filter", "grayscale(0%)")
-      $('#five').css("-webkit-filter", "grayscale(0%)")
-      $('#six').css("-webkit-filter", "grayscale(0%)")
-      $('#seven').css("-webkit-filter", "grayscale(0%)")
-    }
-  )
-});
 // $(document).ready(function() {
-//   $('#two').on({
-//     mouseenter: function() {
-//       $('#img2').css("height", "0px")
-//       $('#img2').css("opacity", "0")
+//   $('figure').hover(
+//     function() {
+//       $(this).css("z-index", "200")
+// 			$(this).css("transform", "scale(1.14)")
+// 			$('#zero').css("-webkit-filter", "grayscale(100%)")
+// 			$('#one').css("-webkit-filter", "grayscale(100%)")
+//       $('#two').css("-webkit-filter", "grayscale(100%)")
+//       $('#three').css("-webkit-filter", "grayscale(100%)")
+//       $('#four').css("-webkit-filter", "grayscale(100%)")
+//       $('#five').css("-webkit-filter", "grayscale(100%)")
+//       $('#six').css("-webkit-filter", "grayscale(100%)")
+//       $('#seven').css("-webkit-filter", "grayscale(100%)")
+//       $('#eight').css("-webkit-filter", "grayscale(100%)")
+// 			$('#nine').css("-webkit-filter", "grayscale(100%)")
+// 			$(this).css("-webkit-filter", "grayscale(0%)")
+//     },
+//     function() {
+//       $(this).css("z-index", "2")
+// 			$(this).css("transform", "scale(1.0)")
+// 			$('#zero').css("-webkit-filter", "grayscale(0%)")
+// 			$('#one').css("-webkit-filter", "grayscale(0%)")
+//       $('#two').css("-webkit-filter", "grayscale(0%)")
+//       $('#three').css("-webkit-filter", "grayscale(0%)")
+//       $('#four').css("-webkit-filter", "grayscale(0%)")
+//       $('#five').css("-webkit-filter", "grayscale(0%)")
+//       $('#six').css("-webkit-filter", "grayscale(0%)")
+//       $('#seven').css("-webkit-filter", "grayscale(0%)")
+//       $('#eight').css("-webkit-filter", "grayscale(0%)")
+// 			$('#nine').css("-webkit-filter", "grayscale(0%)")
+// 			$(this).css("-webkit-filter", "grayscale(0%)")
 //     }
-//   })
+//   )
 // });
-//
-// $(document).ready(function() {
-//   $('#three').on({
-//     mouseenter: function() {
-//       $('#img3').css("height", "75px")
-//       $('#img3').css("opacity", "1")
-//     }
-//   })
-// });
-//
-// $(document).ready(function() {
-//   $('#four').on({
-//     mouseenter: function() {
-//       $('#img4').css("height", "75px")
-//       $('#img4').css("opacity", "1")
-//     }
-//   })
-// });
-//
-// $(document).ready(function() {
-//   $('#five').on({
-//     mouseenter: function() {
-//       $('#img5').css("height", "75px")
-//       $('#img5').css("opacity", "1")
-//     }
-//   })
-// });
-//
-// $(document).ready(function() {
-//   $('#six').on({
-//     mouseenter: function() {
-//       $('#img6').css("height", "75px")
-//       $('#img6').css("opacity", "1")
-//     }
-//   })
-// });
-//
-// $(document).ready(function() {
-//   $('#seven').on({
-//     mouseenter: function() {
-//       $('#img7').css("height", "75px")
-//       $('#img7').css("opacity", "1")
-//     }
-//   })
-// });
-//
-// $(document).ready(function() {
-//   $('#eight').on({
-//     mouseenter: function() {
-//       $('#img8').css("height", "75px")
-//       $('#img8').css("opacity", "1")
-//     }
-//   })
-// });
-// End figcaption animations
 
 // figcaption doesn't show up under 960px. Should get better performance on mobile.
 $(document).ready(function() {
